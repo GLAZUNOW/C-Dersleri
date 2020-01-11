3317bbcfee5d1363ff826f9b65b9401e4d6c0791
Yunus Emre Öztürk <oz2rkk@hotmail.com>
2020-01-11 19:38:23 +0300

    4.Ders

---
+++ cin-ile-input-alma.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	setlocale(LC_ALL,"turkish");
+	int x,y,z;
+	/*cout<<"Birinci sayý: ";
+	cin>>x;
+	cout<<"Ýkinci sayý: ";
+	cin>>y;
+	cout<<"Üçüncü sayý: ";
+	cin>>z;
+	cout<<"Toplam: "<<x+y+z<<endl; */
+	cout<<"Sayýlarý Giriniz: ";
+	cin>>x>>y>>z;
+	cout<<"Toplamlarý: "<<x+y+z<<endl;
+	
+}
