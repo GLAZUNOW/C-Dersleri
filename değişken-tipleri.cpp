54df48e66a14e55f6cd5673ec8915d3a9f9b530b
Yunus Emre Öztürk <oz2rkk@hotmail.com>
2020-01-18 18:07:47 +0300

    11.Ders

---
+++ değişken-tipleri.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+/*	setlocale(LC_ALL, "Turkish"); 
+	int a=32;
+    float b=3.52;
+	double c=4.546;
+	char d='A';
+	bool e=true; 
+	
+	cout<<"a deðiþkeninin deðeri : "<< a <<endl;
+	cout<<"b deðiþkeninin deðeri : "<< b <<endl;
+	cout<<"c deðiþkeninin deðeri : "<< c <<endl;
+	cout<<"d deðiþkeninin deðeri : "<< d <<endl;
+	cout<<"e deðiþkeninin deðeri : "<< e <<endl; */
+	int a,b,c,toplam=0;
+	a=4;
+	b=3;
+	c=5;
+	toplam = a+b+c;
+	cout<<"toplam :"<<toplam;
+}
