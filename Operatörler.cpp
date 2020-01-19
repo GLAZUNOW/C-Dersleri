7b7769585e77ed69be8518141cea9948b5240074
Yunus Emre Öztürk <oz2rkk@hotmail.com>
2020-01-19 17:38:24 +0300

    12.Ders

---
+++ Operatörler.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	setlocale(LC_ALL, "Turkish"); 
+	/*int sayi1=10;
+	int sayi2=4;
+	cout<<"Sayi1+Sayi2 :"<<sayi1+sayi2<<endl;
+	cout<<"Sayi1-Sayi2 :"<<sayi1-sayi2<<endl;
+	cout<<"Sayi1*Sayi2 :"<<sayi1*sayi2<<endl;
+	cout<<"Sayi1/Sayi2 :"<<float(sayi1)/sayi2<<endl;*/
+	int a=5;
+	cout<<"a'nýn deðeri :"<<a<<endl;
+	//a=a+2;
+  	//a+=2;
+  	//a++;
+	//a++;
+	a--;
+	cout<<"a'nýn yeni deðeri :"<<a<<endl;
+}
