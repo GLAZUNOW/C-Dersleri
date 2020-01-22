b300d02fcc2d58c6edbc5f5226e25913741a52dd
Yunus Emre Öztürk <oz2rkk@hotmail.com>
2020-01-22 18:14:12 +0300

    13.Ders

---
+++ stringler.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ 	string strl1 = "Bu bir stringtir.";
+	string strl2 = "Bu da bir stringtir.";
+	string strl3 =  strl1 + strl2 ;
+	
+	cout << "Strl1: "<<strl1<<endl;
+	cout << "Strl2: "<<strl2<<endl;
+	cout << "Strl3: "<<strl3<<endl;
+}
