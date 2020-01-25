e46bbcf301d72859de6616d3cde40ba7288af338
Yunus Emre Öztürk <oz2rkk@hotmail.com>
2020-01-25 21:44:24 +0300

    15.Ders

---
+++ fonsiyon-kullanımı.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+/*void selamla()
+{
+	cout<<"Naber? "<<endl;
+	cout<<"Nasýlsýn? "<<endl;
+}*/
+void selamla();
+int main()
+{
+	setlocale(LC_ALL,"turkish");
+	selamla();
+}
+void selamla()
+{
+	cout<<"Naber? "<<endl;
+	cout<<"Nasýlsýn? "<<endl;
+}
