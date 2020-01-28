eb220e675cb7be36889ecbafa6ec858a761d32e8
Yunus Emre Öztürk <oz2rkk@hotmail.com>
2020-01-28 21:10:20 +0300

    17.Ders

---
+++ swich-case-kullanımı.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL,"turkish");
+	int islem;
+	cout<<"iþlemi giriniz: ";
+	cin>>islem;
+	switch(islem)
+	{
+		case 1:
+			cout<<"1.iþlemi seçtiniz... ";
+			break;
+		case 2:
+			cout<<"2.iþlemi seçtiniz... ";
+			break;
+		case 3:
+			cout<<"3.iþlemi seçtiniz... ";
+			break;
+		case 4:
+			cout<<"4.iþlemi seçtiniz... ";
+			break;
+		case 5:
+			cout<<"5.iþlemi seçtiniz... ";
+			break;
+		
+	}
+}
