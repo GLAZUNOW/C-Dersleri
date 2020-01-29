df37de61bc43e6e614c983b6201fe7f00ff2706d
Yunus Emre Öztürk <oz2rkk@hotmail.com>
2020-01-29 20:42:28 +0300

    18.Ders

---
+++ fonksiyonlarda-return.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int toplama(int a,int b,int c)
+{
+	//cout<<"Toplam: "<<a+b+c<<endl;
+	return a+b+c;
+	cout<<"deneme"<<endl;
+}
+int main()
+{
+	int a=toplama(1,2,3);
+	cout<<"a:"<<a<<endl;
+	setlocale(LC_ALL,"turkish");
+	
+}
