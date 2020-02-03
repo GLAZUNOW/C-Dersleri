61fcddd5f02ff2eeb39fecffcb70633b253429be
Yunus Emre Öztürk <oz2rkk@hotmail.com>
2020-02-03 18:34:47 +0300

    23.Ders

---
+++ p.a.-ve-a.p.f2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL,"turkish");
+/*	int array[]={1,2,3,4};
+	int *ptr=&array[1];
+	cout<<ptr<<endl;*/
+	string array[]={"Mustafa","Murat","Coþkun"};
+/*	string *ptr=array;
+	cout<<ptr+1<<endl;
+	cout<<*(ptr+1)<<endl;
+	cout<<ptr[1]<<endl;*/
+	cout<<array[3]<<endl;
+}
