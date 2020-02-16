1b42ab66dd4b58a110d8b7ecdaafd92786968144
Yunus Emre Öztürk <oz2rkk@hotmail.com>
2020-02-17 00:13:45 +0300

    34.Ders

---
+++ dinamik-bellek-yönetimi.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int *ptr=new int;
+	*ptr=5;
+	delete ptr;
+/*	setlocale(LC_ALL,"turkish");
+	int *ptr;
+	int a[]={1,2,3,4,5};
+	ptr=a;
+	cout<<ptr[1]<<endl;*/
+	
+}
