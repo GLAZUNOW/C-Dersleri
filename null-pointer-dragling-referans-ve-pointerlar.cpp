91b24a30887ba03d2468142f62433f876e4a8543
Yunus Emre Öztürk <oz2rkk@hotmail.com>
2020-02-18 07:05:51 +0300

    35.Ders

---
+++ null-pointer-dragling-referans-ve-pointerlar.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL,"turkish");
+/*	int *ptr= nullptr;
+	ptr=new int;
+	*ptr=10;
+	delete ptr;
+	*ptr =10;
+	cout<<*ptr<<endl;*///hata
+	
+/*	int *ptr1=nullptr;
+	int *ptr2=nullptr;
+	ptr1=new int;
+	*ptr1=10;
+	ptr2=ptr1;
+	delete ptr1;
+	*ptr2=20;
+	cout<<*ptr2<<endl;*///hata
+	
+}
