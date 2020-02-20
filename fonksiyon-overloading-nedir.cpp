bba5bca4d87a04a78b8921702260b64cb7882daf
Yunus Emre Öztürk <oz2rkk@hotmail.com>
2020-02-20 19:04:08 +0300

    37.Ders

---
+++ fonksiyon-overloading-nedir.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+void selamla()
+{
+	cout<<"Merhaba "<<endl;	
+}
+void selamla(string name)
+{
+	cout<<"Merhaba "<<name<<endl;
+}
+void selamla(string name1,string name2)
+{
+	cout<<"Merhaba "<<name1<<","<<name2<<endl;
+}
+int main()
+{
+	setlocale(LC_ALL,"turkish");
+	selamla();
+	selamla("Emre");	
+	selamla("Emre "," Oðuz");
+}
