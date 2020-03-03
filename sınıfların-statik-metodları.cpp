96060826fda4937fda27a301945d1e90ac8fbcac
Yunus Emre Öztürk <oz2rkk@hotmail.com>
2020-03-03 19:57:52 +0300

    47.Ders

---
+++ sınıfların-statik-metodları.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+class Matematik
+{
+	public:
+		static void cube(int x)
+		{
+			cout<<x*x*x<<endl;
+		}
+		static void add2(int x)
+		{
+			cout<<x+2<<endl;
+		}
+};
+int main ()
+{
+	Matematik::cube(3);
+	Matematik::add2(20);
+	setlocale(LC_ALL,"turkish");
+}
