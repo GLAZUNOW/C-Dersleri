45defaed49b9f355d9a8a59b81743cad1f8beb1a
Yunus Emre Öztürk <oz2rkk@hotmail.com>
2020-03-11 07:37:37 +0300

    52.Ders

---
+++ fmod_kullanimi.cpp
@@ -0,0 +1,8 @@
+#include <iostream>      
+#include <math.h>    
+
+int main ()
+{
+  printf ( "fmod of 6.5 / 3 is %f\n", fmod (6.5,3) );
+  printf ( "fmod of 25.7 / 5.1 is %f\n", fmod (25.7,5.1) );
+}
